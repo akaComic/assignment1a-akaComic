d7ebd7fc791c90238f9c99d4436c92c9edc7e12d
Yuchan Chen <122577317+akaComic@users.noreply.github.com>
2024-09-12 19:56:41 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "akaComic";
+    char STUDENT_ID[] = "978398166";
 
     return 0 ;
 }
